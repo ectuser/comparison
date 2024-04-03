4e611b98b236b1408ac2071ebc44a3e38c84a667
ectuser <m.ectuser@gmail.com>
2024-04-03 12:39:49 +0200

    feat: comparison node

--- apps/comparison-node/src/compare-products.ts
+++ apps/comparison-node/src/compare-products.ts
@@ -1,17 +1,27 @@
-// import { useCase } from './di';
+import { ProductInteractor, ProductRepository } from '@product-comparison/product-core';
 
-// const usecase = useCase;
+const repo = new ProductRepository();
+const usecase = new ProductInteractor(repo);
 
-// export async function compareProducts(isins: number[]) {
-//   const products = await usecase.getProductsByIsins(isins);
+export async function compareProducts(isins: number[]) {
+  console.log(isins);
 
-//   const result = products.map(p => {
-//     return {
-//       name: p.name,
-//       isin: p.isin,
-//       greaterCap50: p.cap > 50,
-//     };
-//   })
+  const reqs = await Promise.allSettled(isins.map(isin => usecase.getProduct(isin)));
+  const products = reqs.map(res => {
+    if (res.status === 'fulfilled') {
+      return res.value;
+    }
 
-//   return result;
-// }
+    return null;
+  }).filter(res => res !== null);
+
+  const result = products.map(p => {
+    return {
+      name: p.name,
+      isin: p.isin,
+      greaterCap50: p.cap > 50,
+    };
+  })
+
+  return result;
+}
